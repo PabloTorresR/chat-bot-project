25a243911f22873cb84258a3790ce3b3db4be095
ptorresr <pablo@nomoko.world>
2024-01-21 23:53:35 +0100

    feat: set up message route

---
+++ src/apps/chat-app/backend/routes/message.route.ts
@@ -0,0 +1,11 @@
+import { Express } from 'express';
+import container from '../dependency-injection';
+import { MessagePostController } from '../controllers/MessagePostController';
+
+export const register = (app: Express) => {
+  const messagePostController: MessagePostController = container.get(
+    'Apps.Chatapp.Backend.controllers.MessagePostController',
+  );
+
+  app.post('/messages', messagePostController.run.bind(messagePostController));
+};
