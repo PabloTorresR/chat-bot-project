5bc83fadf4314839888b5463974790bacbda5bf5
ptorresr <pablo@nomoko.world>
2024-01-25 21:35:33 +0100

    fix: routing working

--- src/apps/chat-app/backend/routes/message.route.ts
+++ src/apps/chat-app/backend/routes/message.route.ts
@@ -6,6 +6,5 @@ export const register = (app: Express) => {
   const messagePostController: MessagePostController = container.get(
     'Apps.Chatapp.Backend.controllers.MessagePostController',
   );
-
   app.post('/messages', messagePostController.run.bind(messagePostController));
 };
