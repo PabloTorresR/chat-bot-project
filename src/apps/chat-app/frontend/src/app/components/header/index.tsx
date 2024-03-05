393ab659cd471706368e27edfba29cf807375ab4
Pablo Torres <pablo.torres@check24.de>
2024-03-05 17:20:10 +0100

    fix: user badge good

--- src/apps/chat-app/frontend/src/app/components/header/index.tsx
+++ src/apps/chat-app/frontend/src/app/components/header/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 import { UserBadge } from '@chat-app/features/user/components/user-badge';
 
 const FAKE_IMAGE = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
-const FAKE_USER = 'Paco Gutierrez';
+const FAKE_USER = 'Paco Gutierrez-Sanchez';
 
 const Header = memo(() => (
   <header className={styles.header}>
