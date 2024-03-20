c48b0af9a96daafd23894b4315dcca8edbea0c3a
Pablo Torres <pablo.torres@check24.de>
2024-03-20 13:17:07 +0100

    feat: working chatbot with api key

--- src/apis/llm-service/app.py
+++ src/apis/llm-service/app.py
@@ -1,31 +1,17 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from mangum import Mangum
 from container import Container
 from core.fastapi.routes import add_routes
 from modules.message.application.answer_message import api as answer_message_api
 from modules.message.application import router as message_router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 add_routes([message_router], app)
 container = Container()
 container.wire(modules=[answer_message_api])
-
-
-@app.get("/", include_in_schema=False)
-def welcome():
-    html_content = """
-    <html>
-        <head>
-            <title>Welcome to the Integration API</title>
-        </head>
-        <body>
-            <h1>Welcome to the Integration API</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
-
 
 handler = Mangum(app)
