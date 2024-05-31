d63f74a186ebd44466c2866a90431182c7b1b31c
Pablo Torres <pablo.torres@check24.de>
2024-05-31 14:02:14 +0200

    fix: routes bundeable

--- src/apps/chat-app/backend/routes/status.route.ts
+++ src/apps/chat-app/backend/routes/status.route.ts
@@ -2,10 +2,11 @@ import { Express } from 'express';
 import container from '../dependency-injection';
 import { StatusGetController } from '../controllers/StatusGetController';
 
-export const register = (app: Express) => {
+const register = (app: Express) => {
   const statusGetController: StatusGetController = container.get(
     'Apps.Chatapp.Backend.controllers.StatusGetController',
   );
 
   app.get('/status', statusGetController.run.bind(statusGetController));
 };
+export default { register };
