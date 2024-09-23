8aafb8918e460d9148553f7e29bdd36d736e15ca
Pablo Torres <pablo.torres@check24.de>
2024-09-23 10:13:05 +0200

    fix: better fallback text

--- src/apps/chat-app/frontend/src/app/layouts/mobile-fallback/index.tsx
+++ src/apps/chat-app/frontend/src/app/layouts/mobile-fallback/index.tsx
@@ -7,7 +7,7 @@ const MobileFallback = memo(() => {
     <div className={styles.mobileFallback__overlay}>
       <div className={styles.mobileFallback__container}>
         <h1>Upsss! mobile version coming soon</h1>
-        <h2>Please, try again with device with bigger screen</h2>
+        <h2>Please, try again with any device with a bigger screen</h2>
       </div>
     </div>
   );
